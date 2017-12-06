d23c87d7935777f46b15ff9ddfa6cf0a7772fd6a
mohamed123 <mohamed123@github.com>
2017-12-06 15:30:21 +0200

    slide Show

--- js/slider.js
+++ js/slider.js
@@ -6,36 +6,43 @@
     Author URI      : https://themeforest.net/user/m_alaa     
     Author Email    : mohamed_ll14@yahoo.com
     Author Gmail    : mohamedalaaabas93@gmail.com 
-*/
-$(function () {
-    "use strict";    
-    var slider         = $('.slider'),
-        lengthDiv      = $('.slider').children().length,        
-        childernForDiv = $('.slider').children();
-       
-        $('#next').on('click', function () {        	
-        	var activeClass    = $('.slider .active');
-        	nextSlider(activeClass) ; 	
-        });
-        
-        $('#prev').on('click', function () {        	
-        	var activeClass    = $('.slider .active');        	
-        	prevSlider(activeClass);        	
-        });
-        
-        function nextSlider(activeClass){        	
-        	activeClass.removeAttr('class').next().addClass('active');        	  
-        	if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
-        		slider.children().first().addClass('active');
-        	}
-        }
-        function prevSlider(activeClass){        	
-        	activeClass.removeAttr('class').prev().addClass('active');         	       	  
-        	if($('.slider .active').prev().length == 0 && $('.slider .active').length == 0){
-        		slider.children().last().addClass('active');
-        	} 
-        }
-});
+*/    
+var slider         = $('.slider'),
+    lengthDiv      = $('.slider').children().length,        
+    childernForDiv = $('.slider').children();
+   
+    $('#next').on('click', function () {        	
+    	var activeClass    = $('.slider .active');
+    	nextSlider(activeClass) ; 	
+    });
+    
+    $('#prev').on('click', function () {        	
+    	var activeClass    = $('.slider .active');        	
+    	prevSlider(activeClass);        	
+    });
+    
+    function nextSlider(activeClass){        	
+    	activeClass.removeAttr('class').next().addClass('active');        	  
+    	if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
+    		slider.children().first().addClass('active');
+    	}
+    }
+    function prevSlider(activeClass){        	
+    	activeClass.removeAttr('class').prev().addClass('active');         	       	  
+    	if($('.slider .active').prev().length == 0 && $('.slider .active').length == 0){
+    		slider.children().last().addClass('active');
+    	}     	    
+    }
+    
+    function SlideShow(){    		
+    		console.log('vvv');
+    		$('.slider .active').removeAttr('class').next().addClass('active');
+    		if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
+    			slider.children().first().addClass('active');
+    		}
+    	}
+    var show = setInterval(SlideShow, 2200);
+
 
 
 
