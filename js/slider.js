f72440eded6270513f6da631f64183137ed2e1e0
mohamed123 <mohamed123@github.com>
2017-12-06 16:22:34 +0200

    comment And Stop Slider when click for prev or next and change speed for slider

--- js/slider.js
+++ js/slider.js
@@ -2,46 +2,72 @@
 /*jslint node: true */
 /*jshint strict:false */
 /*
-    Author          : Mohamed Alaa 
-    Author URI      : https://themeforest.net/user/m_alaa     
-    Author Email    : mohamed_ll14@yahoo.com
-    Author Gmail    : mohamedalaaabas93@gmail.com 
+    Author             : Mohamed Alaa Abas
+    Author URI         : https://themeforest.net/user/m_alaa     
+    Author Email       : mohamed_ll14@yahoo.com
+    Author Gmail       : mohamedalaaabas93@gmail.com 
+    GitHub Account     : https://github.com/moahmed123
+    version For Plugin : 1.0.1 v
 */    
-var slider         = $('.slider'),
-    lengthDiv      = $('.slider').children().length,        
-    childernForDiv = $('.slider').children();
-   
-    $('#next').on('click', function () {        	
-    	var activeClass    = $('.slider .active');
-    	nextSlider(activeClass) ; 	
-    });
-    
-    $('#prev').on('click', function () {        	
-    	var activeClass    = $('.slider .active');        	
-    	prevSlider(activeClass);        	
-    });
-    
-    function nextSlider(activeClass){        	
-    	activeClass.removeAttr('class').next().addClass('active');        	  
-    	if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
-    		slider.children().first().addClass('active');
-    	}
-    }
-    function prevSlider(activeClass){        	
-    	activeClass.removeAttr('class').prev().addClass('active');         	       	  
-    	if($('.slider .active').prev().length == 0 && $('.slider .active').length == 0){
-    		slider.children().last().addClass('active');
-    	}     	    
-    }
-    
-    function SlideShow(){    		
-    		console.log('vvv');
+(function ( $ ) {
+	
+	$.fn.slideShow = function(speed){
+		var slider         = this,
+	    	lengthDiv      = slider.children().length,        
+	    	childernForDiv = slider.children(),
+	    	show;
+	    	console.log(lengthDiv);
+	   
+	    $('#next').on('click', function () {
+	    	StopSlidShow();        	
+	    	var activeClass    = $('.slider .active');
+	    	nextSlider(activeClass) ; 	
+	    });
+	    
+	    $('#prev').on('click', function () {
+	    	StopSlidShow();   // stop Slide Show       	
+	    	var activeClass    = $('.slider .active');        	
+	    	prevSlider(activeClass);        	
+	    });
+	    	    
+	    $('#next, #prev').on('mouseleave', function () {        				
+			startSlidShow(); // Start Slide Show 
+	    });
+	    
+	    startSlidShow(); // Perant To Start Slide Show When Loading Page . 
+	    
+	    /*************************
+	     ** All Function To Use ** 
+	     **
+	     **/
+	    function nextSlider(activeClass){        	
+	    	activeClass.removeAttr('class').next().addClass('active');        	  
+	    	if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
+	    		slider.children().first().addClass('active');
+	    	}
+	    }
+	    function prevSlider(activeClass){        	
+	    	activeClass.removeAttr('class').prev().addClass('active');         	       	  
+	    	if($('.slider .active').prev().length == 0 && $('.slider .active').length == 0){
+	    		slider.children().last().addClass('active');
+	    	}     	    
+	    }	    
+	    function SlideShow(){    		    		
     		$('.slider .active').removeAttr('class').next().addClass('active');
     		if($('.slider .active').next().length == 0 && $('.slider .active').length == 0){
     			slider.children().first().addClass('active');
     		}
     	}
-    var show = setInterval(SlideShow, 2200);
+	    function StopSlidShow(){
+	    	clearInterval(show);
+	    }
+	    function startSlidShow(){
+	    	show = setInterval(SlideShow, speed);
+	    }		
+	    //show = setInterval(SlideShow, speed);
+	};
+	
+}( jQuery ));
 
 
 
